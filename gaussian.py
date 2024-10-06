832b06f11c855cbd624d39248ce7a396ca0ed928
rily <18514223+tungtylee@users.noreply.github.com>
2024-10-06 18:21:14 +0800

    feat: sta_lta_staticgauss_exp

--- gaussian.py
+++ gaussian.py
@@ -280,6 +280,90 @@ def staticgauss_exp(goldsigma):
                 print("File Missing:", fn)
 
 
+def sta_lta_staticgauss_exp(goldsigma):
+    # reference setting
+    # [120, 600, 4.0, 1.5]
+
+    # candidate param for lunar
+    # [60, 560, 3.25, 0.75]
+    # [120, 720, 3.25, 0.75]
+
+    # candidate param for mars
+    # [60, 400, 4.75, 0.75]
+
+    os.makedirs("algdev_fig", exist_ok=True)
+
+    sta_len_v = [120, 60, 120, 60]
+    lta_len_v = [600, 560, 720, 400]
+    thr_on_v = [4.0, 3.25, 3.25, 4.75]
+    thr_off_v = [1.5, 0.75, 0.75, 0.75]
+
+    ## lunar
+    basedir = "space_apps_2024_seismic_detection"
+    data_directory = basedir + "/data/lunar/training/data/S12_GradeA/"
+    fnlist = gridsearch.read_list_to_fnlist(os.path.join("algdev", "lunar_gtlist.txt"))
+    figfnprefix = os.path.join("algdev_fig", "lunar_")
+
+    for idx, fn in enumerate(fnlist):
+        subfnlist = [fn]
+        for m in range(4):
+            sta_len, lta_len, thr_on, thr_off = (
+                sta_len_v[m],
+                lta_len_v[m],
+                thr_on_v[m],
+                thr_off_v[m],
+            )
+            fnamelist, time_abs_str_list, time_rel_list, alltr, alltrdata = (
+                eval.run_stalta_from_fnlist(
+                    data_directory,
+                    subfnlist,
+                    sta_len,
+                    lta_len,
+                    thr_on,
+                    thr_off,
+                    ext=True,
+                )
+            )
+
+            if len(alltr) > 0:
+                tr = alltr[0]
+                tr_data = alltrdata[0]
+                params = f"params: {sta_len}, {lta_len}, {thr_on}, {thr_off}"
+                figtitle = fn + "\n" + params
+                arrivallist_in_s = time_rel_list
+                figfn = figfnprefix + str(idx) + f"_m{m}_final.png"
+                on_off = fixedgaussian(
+                    tr, tr_data, sigma=goldsigma, step_size=30, span=320
+                )
+                # merged_on_off = merge_intervals(on_off)
+                on_rel = [x[0] for x in on_off]
+                time_abs_str_list_filt, time_rel_list_filt = filter_time_by_on_off(
+                    time_abs_str_list, time_rel_list, on_rel, span=320
+                )
+                viewer.viewer_all(tr, tr_data, figtitle, time_rel_list_filt, figfn)
+            else:
+                print("File Missing:", fn)
+
+
+def filter_time_by_on_off(time_abs_str_list, time_rel_list, on_rel, span=60):
+    # Initialize filtered lists
+    time_abs_str_list_filt = []
+    time_rel_list_filt = []
+
+    # Loop through each relative time and corresponding absolute time
+    for abs_time, rel_time in zip(time_abs_str_list, time_rel_list):
+        # Check if the current relative time falls within any of the on_off_rel ranges
+        matched = any(abs(rel_time - event_time) < span for event_time in on_rel)
+
+        # If matched, add to the filtered lists
+        if matched:
+            time_abs_str_list_filt.append(abs_time)
+            time_rel_list_filt.append(rel_time)
+
+    # Return the filtered absolute and relative times
+    return time_abs_str_list_filt, time_rel_list_filt
+
+
 if __name__ == "__main__":
     print("Perform statistics")
     goldsigma = daywise_plot()
@@ -287,3 +371,5 @@ if __name__ == "__main__":
     dygauss_exp()
     print("Perform staticgauss_exp")
     staticgauss_exp(goldsigma)
+    print("Perform sta_lta_staticgauss_exp")
+    sta_lta_staticgauss_exp(goldsigma)
